dcba4bef0428c6e1de4dcbe324649ad6d27befa2
hcprojectsgit <hcprojectsgit@gmail.com>
2021-03-05 16:18:55 +0530

    four done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,26 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'guidelines',
+    loadChildren: () => import('./guidelines/guidelines.module').then( m => m.GuidelinesPageModule)
+  },
 ];
 
 @NgModule({
